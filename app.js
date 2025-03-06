24b54280e26c47e51a79970dc008604022868b56
SyedAsharBukhari <asharbukhari649@gmai.com>
2025-03-06 11:36:43 -0800

    done

---
+++ app.js
@@ -0,0 +1,19 @@
+
+let userName = ["qirat", "mesham", "qahroba","ashar"];
+
+let rollNum = [255, 288, 655];
+
+let userinp = +prompt("enter your name");
+let usernum = +prompt("enter your number");
+
+if(userName.includes(userinp)){   //includes check karta h k array mn value mojood h ya nhi
+    console.log("Your child is present");
+    
+}
+else if(rollNum.includes(usernum)){
+    console.log("Your child is also present");
+}
+else{
+    console.log("Your child is absent");
+    
+}
