7883cf8e3f63343283c4b9231816854fd910fa49
SyedAsharBukhari <asharbukhari649@gmai.com>
2025-03-07 13:36:42 -0800

    working

--- app.js
+++ app.js
@@ -1,30 +1,77 @@
 
-let userName = ["qirat", "mesham", "qahroba","ashar"];
+// let userName = ["qirat", "mesham", "qahroba","ashar"];
 
-let rollNum = [255, 288, 655];
+// let rollNum = [255, 288, 655];
 
-let userinp = +prompt("enter your name");
-let usernum = +prompt("enter your number");
+// let userinp = +prompt("enter your name");
+// let usernum = +prompt("enter your number");
 
-if(userName.includes(userinp)){   //includes check karta h k array mn value mojood h ya nhi
-    console.log("Your child is present");
+// if(userName.includes(userinp)){   //includes check karta h k array mn value mojood h ya nhi
+//     console.log("Your child is present");
     
-}
-else if(rollNum.includes(usernum)){
-    console.log("Your child is also present");
-}
-else{
-    console.log("Your child is absent");
+// }
+// else if(rollNum.includes(usernum)){
+//     console.log("Your child is also present");
+// }
+// else{
+//     console.log("Your child is absent");
     
-}
+// }
 
 
-let std = "ali";
-let userinp1 = prompt("name")
+// let std = "ali";
+// let userinp1 = prompt("name")
 
-if(std == userinp ){
-    console.log("Your child is present");
+// if(std == userinp ){
+//     console.log("Your child is present");
+    
+// }else{
+//     console.log("Your child is absent");
+// }
+
+
+// _______________________________________________new task
+
+
+let eng = 20
+let math = 18
+let sci = 22
+let urd = 19
+let totalMarks = 100;
+
+let total = eng + math + sci + urd;
+console.log(total*100/1000);
+console.log(  total % totalMarks );
+
+
+let email = "qirat@12";
+let pass = "qirr444";
+let num = 2345;
+let useinp = prompt("enter your Email")
+
+// let useinp2 = prompt("enter your information")
+
+
+
+if (useinp === email) {
+    console.log("correct email");
     
 }else{
-    console.log("Your child is absent");
+    console.log("Incorrect email");
+} 
+
+let useinp1 = prompt("enter your password")
+
+if(useinp1 === pass){
+    console.log("correct password");
+}else{
+    console.log("Incorrect password");
+} 
+
+let useinp2 = +prompt("enter your number")
+
+if(useinp2 === num){
+    console.log("correct number");
+}else{
+    console.log("Incorrect number");
 }
